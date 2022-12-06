0c5f9bdedcaa0c7947baf2daf239082a898fbc62
afolabiopakunle <opakunleafolabi@gmail.com>
2022-12-06 11:04:05 +0100

    mats-tab module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MaterialsModule } from './materials/materials.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MaterialsModule ],
   declarations: [ AppComponent, ],
   bootstrap:    [ AppComponent ]
 })
